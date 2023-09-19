9b6192e47a385cfe68c2a74dd619b4ac670b92b0
DELLL-5577\huihui <wf021325@gmail.com>
2023-09-19 23:47:06 +0800

    no message

---
+++ bfg.bat
@@ -0,0 +1,5 @@
+java -jar bfg-1.14.0.jar --delete-files bfg.bat
+java -jar bfg-1.14.0.jar --delete-files bfg.txt
+java -jar bfg-1.14.0.jar --delete-files hh.txt
+java -jar bfg-1.14.0.jar --delete-files storm.js
+java -jar bfg-1.14.0.jar --delete-files caiyun.js
