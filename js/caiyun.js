874662c2141541a7d7dcd309b8e897bd4e674524
wf021325@gmail.com <wf@gmail.com>
2023-10-21 23:12:41 +0800

    no message

---
+++ js/caiyun.js
@@ -0,0 +1,58 @@
+/*
+彩云天气
+
+====================================
+[rewrite_local]
+# 普通版广告
+^https:\/\/ad\.cyapi\.cn\/v2\/req\?app_name=weather url reject-dict
+# 赏叶赏花
+^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\?app_name=weather url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+# 解锁vip
+^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+# 卫星云图 48小时预报
+^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+
+[mitm]
+hostname = *.cyapi.cn
+====================================
+ */
+var huihui = {},
+    url = $request.url;
+if (url.includes("/v2/user")) {
+    let obj = JSON.parse($response.body);
+    obj.result.is_vip = !0,
+        obj.result.svip_expired_at = 3742732800,
+        obj.result.vip_type = "s",
+        huihui.body = JSON.stringify(obj)
+}
+if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
+    huihui.headers = $request.headers;
+    huihui.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
+}
+if(url.includes('v1/activity')){
+	let body = $response.body
+	body = '{"status":"ok","activities":[{"items":[{}]}]}';
+	huihui.body = body;
+}
+$done(huihui);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY"; 
