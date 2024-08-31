ad86c656413cd6a7088209a8f430f07ffc1bf88a
DELL-5577\huihui <wf021325@gmail.com>
2024-09-01 00:39:37 +0800

    no message

--- js/caiyun.js
+++ js/caiyun.js
@@ -11,9 +11,9 @@
 ^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
 # 卫星云图 48小时预报
 ^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
-
 # 7.20.0版本显示VIP
 ^https?:\/\/biz\.cyapi\.cn\/api\/v1\/user_detail$ url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+
 [mitm]
 hostname = *.cyapi.cn
 ====================================
@@ -28,9 +28,12 @@ if (url.includes("/v2/user")) {
         huihui.body = JSON.stringify(obj)
 }
 if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
     huihui.headers = $request.headers;
-    huihui.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjYyNzQxMzVkYWM3MGMwMDE4YzFlNDBmIiwidXNlcl9pZCI6IjVmNWJmYzU3ZDJjNjg5MDAxNGUyNmJiOCIsInZlcnNpb24iOjIsImV4cCI6MTcyMTYyNDYyOSwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE3MTM4NDg2MjksInN2aXBfZXhwaXJlZF9hdCI6MTg1NjY4NTAzMSwicHJpbWFyeSI6dHJ1ZX0.bBT3vbfATa-LF1G34j4VjPTYtwcKHfG3oHIkFlmg1dY';
-	//Token来源：@https://t.me/chxm1023_Chat    https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
+    huihui.headers['device-token'] = token;
+    if (compareVersions(huihui.headers.version, '7.1.9') > 0) {
+        huihui.headers.Authorization = 'Bearer ' + token;
+    }
 }
 if(url.includes('v1/activity')){
 	let body = $response.body
@@ -44,6 +47,8 @@ if (url.includes('/user_detail')) {
     huihui.body = JSON.stringify(obj)
 }
 $done(huihui);
+
+function compareVersions(t,r){const e=t.split(".").map(Number),n=r.split(".").map(Number);for(let t=0;t<Math.max(e.length,n.length);t++){const r=e[t]||0,i=n[t]||0;if(r>i)return 1;if(r<i)return-1}return 0}
 
 
 
