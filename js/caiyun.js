2c85b81ac06e9e8b03fb7d2b68c05527d2727ae1
DELL-5577\huihui <wf021325@gmail.com>
2024-09-02 18:13:51 +0800

    no message

--- js/caiyun.js
+++ js/caiyun.js
@@ -2,6 +2,10 @@
 彩云天气
 
 ====================================
+[filter_local]
+# 上传信息
+host, gather.colorfulclouds.net ,
+
 [rewrite_local]
 # 7.1.9 限时福利Svip
 ^https:\/\/biz\.cyapi\.cn\/p\/v1\/trial_card\/info url reject-dict
@@ -15,43 +19,32 @@
 ^https?:\/\/biz\.cyapi\.cn\/api\/v1\/user_detail$ url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
 
 [mitm]
-hostname = *.cyapi.cn
+hostname = biz.cyapi.cn, wrapper.cyapi.cn
 ====================================
  */
-var huihui = {}, url = $request.url;
+var huihui = {}, url = $request.url, headers = ObjectKeys2LowerCase($request.headers);
 if (url.includes("/v2/user")) {
     let obj = JSON.parse($response.body);
-    obj.result.is_vip = !0;
-    obj.result.svip_expired_at = 3742732800;
-    obj.result.vip_type = "s";
+    Object.assign(obj.result, {is_vip: true, svip_expired_at: 3742732800, vip_type: "s"});
     huihui.body = JSON.stringify(obj)
-} else if (/v1\/(satellite|nafp\/origin_images)/g.test(url)) {
+} else if (/v1\/(satellite|nafp\/origin_images)/.test(url)) {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
-    huihui.headers = $request.headers;
-    huihui.headers['device-token'] = token;
-    if (compareVersions(huihui.headers.version, '7.19.0') > 0) {
-        huihui.headers.Authorization = 'Bearer ' + token;
+    huihui.headers = {...headers, 'device-token': token};
+    if (compareVersions(headers['app-version'], '7.19.0') > 0) {
+        huihui.headers['authorization'] = 'Bearer ' + token;
     }
 } else if (url.includes('v1/activity')) {
-    let body = $response.body;
-    const headers = $request.headers;
-    const appversion = headers['app-version'];
-    if (compareVersions(appversion, '7.20.0') < 0) {
-        body = '{"status":"ok","activities":[{"items":[{}]}]}';
-    } else {
-        body = '{"status":"ok","activities":[]}';
-    }
-    huihui.body = body
+    const body = compareVersions(headers['app-version'], '7.20.0') < 0 ? '{"status":"ok","activities":[{"items":[{}]}]}' : '{"status":"ok","activities":[]}';
+    huihui.body = body;
 } else if (url.includes('/user_detail')) {
     const obj = JSON.parse($response.body);
-    obj.vip_info.svip.is_auto_renewal = true;
-    obj.vip_info.svip.expires_time = '3742732800';
+    Object.assign(obj.vip_info.svip, {is_auto_renewal: true, expires_time: '3742732800'});
     huihui.body = JSON.stringify(obj)
 }
 $done(huihui);
 
 function compareVersions(t,r){"string"!=typeof t&&(t="0"),"string"!=typeof r&&(r="0");const e=t.split(".").map(Number),n=r.split(".").map(Number);for(let t=0;t<Math.max(e.length,n.length);t++){const r=e[t]||0,i=n[t]||0;if(r>i)return 1;if(r<i)return-1}return 0}
-
+function ObjectKeys2LowerCase(obj){return Object.fromEntries(Object.entries(obj).map(([k,v])=>[k.toLowerCase(),v]))};
 
 
 
